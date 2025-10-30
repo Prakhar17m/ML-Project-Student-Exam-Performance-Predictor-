a7ad0542cdb90fff1cfe39b2564961c1fc0456be
Prakhar Mehrotra <131417300+Prakhar17m@users.noreply.github.com>
2025-10-31 04:31:47 +0530

    commit
    Removed debug mode from app.run() in application.py

--- application.py
+++ application.py
@@ -45,4 +45,4 @@ def predict_datapoint():
             # show minimal error message in UI for now
             return render_template('home.html', error=str(e))
 if __name__=="__main__":
-    app.run(host="0.0.0.0", debug=True)        
+    app.run(host="0.0.0.0")        
